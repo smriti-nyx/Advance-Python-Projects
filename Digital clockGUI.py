9abd293e19a601097754d042f6ddfffa53fa6f48
smriti-nyx <smriticodedthis@gmail.com>
2026-05-26 14:25:21 +0530

    Digital clock GUI using Tkinter

--- Digital clockGUI.py
+++ Digital clockGUI.py
@@ -7,7 +7,7 @@ window = tk.Tk()
 window.title("My first window")
 
 #adding a label : a sticky note for the window I created above
-label = tk.Label(window , text = "Hello!" , font = ("Arial" , 40) , bg = "black" ,fg = "lime")
+label = tk.Label(window ,  text = "Hello!" , font = ("Arial" , 40) , bg = "black" ,fg = "lime")
 label.pack()
 
 window.mainloop()
