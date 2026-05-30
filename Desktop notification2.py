63fe921bb2a2c85ae196f6345bcc739c12ad2f39
smriti-nyx <smriticodedthis@gmail.com>
2026-05-30 21:12:24 +0530

    Digital clock GUI using Tkinter

--- Desktop notification2.py
+++ Desktop notification2.py
@@ -1,5 +1,8 @@
-from pync import Notifier
-import tkinter as tk
+from plyer import notification
 
-def send_notification():
-    
+notification.notify(
+    title = "Hello",
+    message = "My first desktop notification",
+    app_name ="My python app",
+    timeout = 6 #seconds before the notification disappears
+)
